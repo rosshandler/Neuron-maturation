7f6845b3dd3f93871aa535b26f779f95a92595e7
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2022-07-25 11:16:41 +0100

    Create getGeneInfo.R

---
+++ MouseBrainDevAtlas/R/getGeneInfo.R
@@ -0,0 +1,10 @@
+# Install biomart bioconductor packages
+
+# Save excel gene list (Feline's) file as csv (make sure only rows and columns are saved in the file).
+
+# Read csv file into R
+
+# load biomart, get gene ids and extract ensembl and chromosomes for each gene symbol
+
+# write a function that takes any gene list of symbols and returns the corresponding ensembl ids and chromosomes (a character vector)
+
