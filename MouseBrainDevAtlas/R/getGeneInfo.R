8cd9af0262b6643cdcfc98785e765433af4e77a6
hs763 <109690308+hs763@users.noreply.github.com>
2022-07-26 14:00:11 +0100

    Update getGeneInfo.R

--- MouseBrainDevAtlas/R/getGeneInfo.R
+++ MouseBrainDevAtlas/R/getGeneInfo.R
@@ -1,10 +1,23 @@
 # Install biomart bioconductor packages
+library(biomaRt)
+ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
+setwd('/Users/hannaszafranska/programming')
 
 # Save excel gene list (Feline's) file as csv (make sure only rows and columns are saved in the file).
 
 # Read csv file into R
+data <- read.csv('Selected_genes0.csv')
 
 # load biomart, get gene ids and extract ensembl and chromosomes for each gene symbol
+gene_tab <- getBM(attributes = c('hgnc_symbol', 'ensembl_gene_id', 'chromosome_name'),
+      filters = 'hgnc_symbol',
+      values = data$Gene, 
+      mart = ensembl)
 
 # write a function that takes any gene list of symbols and returns the corresponding ensembl ids and chromosomes (a character vector)
+getBM(attributes = c('hgnc_symbol', 'ensembl_gene_id', 'chromosome_name'),
+      filters = 'hgnc_symbol',
+      values = 'HOMER1', 
+      mart = ensembl)
 
+setdiff(data$Gene,gene_tab$hgnc_symbol)
