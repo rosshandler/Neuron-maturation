b1114fc110d9c72eb0c1aea7067727d1057e0726
hs763 <109690308+hs763@users.noreply.github.com>
2022-07-26 14:02:58 +0100

    Update getGeneInfo.R

--- MouseBrainDevAtlas/R/getGeneInfo.R
+++ MouseBrainDevAtlas/R/getGeneInfo.R
@@ -9,15 +9,11 @@ setwd('/Users/hannaszafranska/programming')
 data <- read.csv('Selected_genes0.csv')
 
 # load biomart, get gene ids and extract ensembl and chromosomes for each gene symbol
+
+# write a function that takes any gene list of symbols and returns the corresponding ensembl ids and chromosomes (a character vector)
 gene_tab <- getBM(attributes = c('hgnc_symbol', 'ensembl_gene_id', 'chromosome_name'),
       filters = 'hgnc_symbol',
       values = data$Gene, 
-      mart = ensembl)
-
-# write a function that takes any gene list of symbols and returns the corresponding ensembl ids and chromosomes (a character vector)
-getBM(attributes = c('hgnc_symbol', 'ensembl_gene_id', 'chromosome_name'),
-      filters = 'hgnc_symbol',
-      values = 'HOMER1', 
       mart = ensembl)
 
 setdiff(data$Gene,gene_tab$hgnc_symbol)
